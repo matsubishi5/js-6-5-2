e0f0868ad87547c844cf38f725ab688b8a7ba6a0
TakumiMatsuo <user@matsuotakumi-no-MacBook-Air.local>
2020-03-09 14:13:05 +0900

    [Start]6-5

---
+++ app.js
@@ -0,0 +1,23 @@
+// $(function(){
+// 	$('.box1').css({
+// 		'background-color':'#0000FF',
+// 		'height':'100px'
+// 	});
+// });
+
+// $(function(){
+// 	$('.box1').slideDown();
+// });
+
+// $(function(){
+// 	$('.box1').slideUp();
+// });
+
+// $(function(){
+// 	$('.box1').show();
+// 	$('.box1').css({'background-color':'#0000FF'});
+// });
+
+$(function(){
+	$('.box1').hide();
+});
